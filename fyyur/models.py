e97143a7cb834c3edd81e5d397ef9d68a461e7ff
Amr <amraljmal14@gmail.com>
2021-05-24 15:17:43 +0300

    add trivia

--- fyyur/models.py
+++ fyyur/models.py
@@ -51,8 +51,8 @@ class Artist(db.Model):
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
-  __tablename__ = "Show"
-  id = db.Column(db.Integer,primary_key = True)
-  start_time = db.Column(db.String(),nullable = False)
-  artist_id= db.Column(db.Integer, db.ForeignKey('Artist.id'),nullable=False)
-  venue_id = db.Column(db.Integer,db.ForeignKey('Venue.id'),nullable = False)
+    __tablename__ = "Show"
+    id = db.Column(db.Integer,primary_key = True)
+    start_time = db.Column(db.String(),nullable = False)
+    artist_id= db.Column(db.Integer, db.ForeignKey('Artist.id'),nullable=False)
+    venue_id = db.Column(db.Integer,db.ForeignKey('Venue.id'),nullable = False)
