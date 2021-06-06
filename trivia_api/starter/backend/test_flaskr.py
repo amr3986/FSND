54e80e7284cac2385105dd59b02b7b4894f39205
Amr <amraljmal14@gmail.com>
2021-06-06 09:58:10 +0300

    add more test cases

--- trivia_api/starter/backend/test_flaskr.py
+++ trivia_api/starter/backend/test_flaskr.py
@@ -60,35 +60,73 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['deleted'])
 
+    def test_post_question(self):
+        res = self.client().post("/questions", json=self.new_question)
+        data = json.loads(res.data)
 
-        def test_delete_question_422(self):
-            res = self.client().delete("/questions/200")
-            data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
 
-            self.assertEqual(res.status_code, 422)
-            self.assertEqual(data["success"], False)
-            self.assertEqual(data["message"], "The request you made was not processable.")
-            def test_get_categories(self):
-                res = self.client().get('/categories')
-                data = json.loads(res.data)
-                self.assertEqual(res.status_code, 200)
-                self.assertEqual(data['success'], True)
+    
+    def test_search_questions(self):
+        res = self.client().post("/questions/search", json={"searchTerm": "title"})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+    def test_search_questions_404(self):
+        res = self.client().post("/questions/search", json={"search": "1"})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "The resource you requested was not found.")
+
+    def test_questions_with_category_id(self):
+        res = self.client().get("/categories/1/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["current_category"], 2)
+
+
+    def test_delete_question_422(self):
+        res = self.client().delete("/questions/200")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "The request you made was not processable.")
+
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
             
 
-        def test_get_paginated_questions(self):
-            res = self.client().get('/questions')
-            data = json.loads(res.data)
-            self.assertEqual(res.status_code, 200)
-            self.assertEqual(data['success'], True)
-            self.assertTrue(data['total_questions'])
-            self.assertTrue(data['categories'])
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+        
+    def test_questions_with_category_id_not_found(self):
+        res = self.client().get("/questions/100")
+        data = json.loads(res.data)
 
-        def test_get_paginated_questions_404(self):
-            res = self.client().get('/questions?page=1000')
-            data = json.loads(res.data)
-            self.assertEqual(res.status_code, 404)
-            self.assertEqual(data['success'], False)
-            self.assertEqual(data['message'], 'The resource you requested was not found.')
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "This method is not allowed, sorry.")
+
+    def test_get_paginated_questions_404(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'The resource you requested was not found.')
 
 
 
